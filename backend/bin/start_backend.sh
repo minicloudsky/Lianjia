36d063271b48153d151926f99755eb914a09e208
minicloudsky <1397991131@qq.com>
2020-07-20 00:28:57 +0800

    完成首页前端开发

--- backend/bin/start_backend.sh
+++ backend/bin/start_backend.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/python
+source /home/.jywcode/lianjia/backend/venv/bin/activate
 
-/home/lck/software/pyenv/versions/3.6.6/bin/gunicorn -w 2 --forwarded-allow-ips=* --bind=0.0.0.0:1126 noah_opendata.wsgi:application --chdir /home/lck/winshared/apps/devops/noah_opendata/ --access-logfile /tmp/apollo-backend-access.log --error-logfile /tmp/apollo-backend-error.log 
+gunicorn -w 2 --forwarded-allow-ips=* --bind=0.0.0.0:1126 lianjia.wsgi:application --chdir /home/.jywcode/lianjia/backend
+ --access-logfile /tmp/lianjia-access.log --error-logfile /tmp/lianjia-error.log
